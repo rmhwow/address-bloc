6821eb5e1f120be4ec2dde42764c2596066e69a5
Morgan OLeary <rmhwow@gmail.com>
2015-07-03 12:15:32 -0400

    added new tests and csv file

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -85,5 +85,29 @@
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
 
      end
+      it "imports the correct number of entries" do
+       book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
+ 
+       expect(book_size).to eql 3
+     end
+   it "imports the 1st entry" do
+       book.import_from_csv("entries_2.csv")
+       p "*********************"
+       p book.entries
+       entry_one = book.entries[0]
+       check_entry(entry_one,"Brigid","920-851-0457", "brigid.k.oleary@gmail.com")
+     end
+       it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Caitlin", "920-858-9596", "caitlin.g.oleary@gmail.com")
+     end
+ 
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+        entry_three = book.entries[2]
+       check_entry(entry_three, "Morgan", "920-858-9594", "rmhwow@yahoo.com" )
+     end
    end
  end
